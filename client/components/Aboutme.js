547ca5e347fd565817325d04c57d2c10ec52f856
Patrick Kenny <patrickmkenny91@gmail.com>
2023-07-25 10:45:27 -0500

    hello

--- client/components/Aboutme.js
+++ client/components/Aboutme.js
@@ -15,7 +15,6 @@ export const Aboutme = props => {
 
       <p>"I recently made the difficult decision to change careers and pursue my passion for app development. I recently graduated from  the Fullstack Academy Software Development program, where I honed my app development skills and learned about front-end, back-end, and data systems.</p>
 
-      <p>I am looking for opportunities to work on exciting app development projects and continue growing as a software engineer. It's extremely exciting to be in an industry that is always changing and provides endless room to grow!"</p>
       </div>
       </div>
   
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,49 +1,85 @@
-import React, { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
-import anime from 'animejs'
-import gsap from 'gsap'
-import ReactCSSTransitionGroup from 'react-transition-group'
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import anime from "animejs";
+import gsap from "gsap";
+import ReactCSSTransitionGroup from "react-transition-group";
 /**
  * COMPONENT
  */
 export const Home = ({ firstRender, setFirstRender }) => {
-
-
-
-
-
-
   useEffect(() => {
-    console.log(firstRender)
-    if (firstRender) {
-      const tl = gsap.timeline(({}))
-      tl.from('#home-logo', { x: 500, duration: 1 })
-      tl.from('#home-quote', { y: 500, duration: 1 })
-      tl.from('#home-profpic', { opacity: 0, duration: 1 })
-      tl.play()
-      setFirstRender(false)
-    }
+    console.log("timeline running");
+    const tl1 = gsap.timeline({ delay: 0.5 });
 
+    tl1.to(".home-text", {
+      y: 20,
+      visibility: "visible",
+      autoAlpha: 1.0,
+      duration: 1,
+      ease: "elastic",
+      stagger: 0.1,
+    });
 
-  }, [])
+    tl1.from(
+      "#paint-splatter",
+      { x: 50, autoAlpha: 0, duration: 1, ease: "elastic", stagger: 0.1 },
+      "-=1.5"
+    );
 
-
-
-
+    tl1.from(
+      "#blackwhiteprof",
+      { x: -50, autoAlpha: 0, duration: 1, ease: "elastic", stagger: 0.1 },
+      "-=1.5"
+    );
+  }, []);
 
   return (
+    <div className="home-container">
+      <div className="left home-left-container">
+        <div id="home-text-container">
+          <div className="line-1 home-text">Hi, my name is </div>
+          <div className="line-2 home-text">Pat Kenny.</div>
+          <div className="line-3 home-text">Full Stack Developer</div>
+          <div className="line-4 home-text">
+            Chicago kid pursuing his passion for technology.
+          </div>
 
-      <div id='home-container' >
-        <img id='home-logo' src='pkdev.png' />
-          {/* <div id='home-quote'>JUST CODE IT.</div> */}
+          <div className="line-6 home-text">
+            “You miss 100% of the shots you don’t take”
+          </div>
+          <div className="line-7 home-text">-Wayne Gretzky</div>
+          <div onClick={(e) => clickHandler()} className="line-8 home-text">
+            -Michael Scott
+          </div>
+        </div>
       </div>
-
-  )
-}
+      <div className="right home-right-container">
+        <div className="photo-paint-container">
+          <img
+            className=""
+            id="paint-splatter"
+            alt="paint-splatter"
+            height="360"
+            width="360"
+            src="/paint-splatter.png"
+          />
+          <img
+            priority="true"
+            className=""
+            id="blackwhiteprof"
+            alt="blackwhiteprof"
+            height="360"
+            width="280"
+            src="/blackwhiteprof.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 /**
  * CONTAINER
  */
 
-
-export default Home
+export default Home;
